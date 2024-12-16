cd902d5ad21722390ffb97b2eb056e841c95928f
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-16 14:33:15 -0500

    Update Play-mp3.js

--- plugins/Play-mp3.js
+++ plugins/Play-mp3.js
@@ -13,27 +13,6 @@ https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 */
 
-
-// *[ ❀ YTMP4 ]*
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m, rcanal)
-    
-try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
-let dl_url = api.data.dl
-
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
-} catch (error) {
-console.error(error)
-}}
-
-handler.command = ['ytmp4']
-
-export default handler
-
-
 // *[ ❀ YTMP3 ]*
 import fetch from 'node-fetch'
 
