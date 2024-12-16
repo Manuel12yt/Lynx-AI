cebdbea99099924882a4cad2cc1e2a51e7378279
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-16 16:58:57 -0500

    Update registro.js

--- plugins/registro.js
+++ plugins/registro.js
@@ -26,7 +26,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
       txt += `│  ❄️  *EDAD* : ${age} años\n`
       txt += `│  ❄️  *NUMERO DE SERIE*\n`
       txt += `└  ❄️  ${sn}`
-await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m,fake)
+await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
 await m.react('✅')
 }
 handler.help = ['reg'].map(v => v + '*<nombre.edad>*')
