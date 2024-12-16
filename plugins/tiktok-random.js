d77652ec7cc38e94e6ce8bf2aab9281cd70fa4ec
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-16 11:58:41 -0500

    Update tiktok-random.js

--- plugins/tiktok-random.js
+++ plugins/tiktok-random.js
@@ -31,7 +31,6 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 
   if (!searchQuery) {
     searchQuery = query[Math.floor(Math.random() * query.length)];
-    m.reply('👀 *No se especificó un término, buscando un video aleatorio...*');
   } else {
     m.reply(`👀 *Buscando videos sobre: ${searchQuery}*`);
   }
