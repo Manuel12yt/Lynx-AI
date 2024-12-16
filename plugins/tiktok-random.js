76574789ca73fe5f3b519a6cc61445abbdd9cf7d
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-16 12:27:04 -0500

    Update tiktok-random.js

--- plugins/tiktok-random.js
+++ plugins/tiktok-random.js
@@ -52,7 +52,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 
 handler.help = ['tiktok <consulta>'];
 handler.tags = ['downloader'];
-handler.command = /^(tiktokrandom|tkrandom)$/i;
+handler.command = ['tiktokrandom','tkrandom'];
 handler.limit = true;
 handler.register = true;
 
