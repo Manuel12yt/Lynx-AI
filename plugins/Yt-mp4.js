333a219f13b42db1e35e053a3a46c7f9207556fa
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-16 18:52:23 -0500

    Update Yt-mp4.js

--- plugins/Yt-mp4.js
+++ plugins/Yt-mp4.js
@@ -17,12 +17,13 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m, rcanal)
-    
+await m.react('🕓')    
 try {
 let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
 let dl_url = api.data.dl
 
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
+await m.react('')
 } catch (error) {
 console.error(error)
 }}
