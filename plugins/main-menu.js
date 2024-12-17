2bc123f48e4fa5d8718e6680f249d18a198eaa7e
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-16 20:59:10 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -117,7 +117,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 
     const text = menu.replace(new RegExp(`%(${Object.keys(replace).join('|')})`, 'g'), (_, key) => replace[key] || '');
     await m.react('🌟');
-    await conn.sendFile(m.chat, imagen1, 'thumbnail.jpg', text.trim(), m);
+    await conn.sendFile(m.chat, imagen1, 'thumbnail.jpg', text.trim(), m,rcanal);
 
   } catch (e) {
     console.error('Error al generar el menú:', e);
