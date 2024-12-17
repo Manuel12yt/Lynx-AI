49fed6b6a75cf520e7e32a25737448c1d258f6dc
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-16 19:34:13 -0500

    Update Yt-mp4.js

--- plugins/Yt-mp4.js
+++ plugins/Yt-mp4.js
@@ -1,35 +1,28 @@
-/* 
-[ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
 // *[ ❀ YTMP4 ]*
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m, rcanal)
-await m.react('🕓')    
-try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
-let dl_url = api.data.dl
+  if (!text) return conn.reply(m.chat, `❀ Ingresa un link de YouTube válido`, m);
 
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
-await m.react('✅')
-} catch (error) {
-console.error(error)
-}}
+  await m.react('🕓');
 
-handler.help = ['ytmp4 *<url>*']
-handler.tags = ['downloader']
-handler.command = ['ytmp4']
+  try {
+    let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json();
+    let dl_url = api.data.dl;
+
+    await conn.sendFile(m.chat, dl_url, 'video.mp4',m,listo,fake);
+
+    await m.react('✅');
+  } catch (error) {
+    console.error(error);
+    await m.react('❌');
+    conn.reply(m.chat, `❀ Ocurrió un error al intentar descargar el video. Inténtalo más tarde.`, m);
+  }
+};
+
+handler.help = ['ytmp4 *<url>*'];
+handler.tags = ['downloader'];
+handler.command = ['ytmp4'];
 handler.register = true;
-export default handler
+
+export default handler;
