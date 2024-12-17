067fb4515c50d4f65d50e8cd495546da2bca39d8
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-17 12:46:45 -0500

    Update chatgpt-ia.js

--- plugins/chatgpt-ia.js
+++ plugins/chatgpt-ia.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
   if (!text) {
-    return conn.reply(m.chat, `❀ Por favor, ingresa un texto para realizar la consulta.`, m);
+    return conn.reply(m.chat, `❀ Por favor, ingresa un texto para realizar la consulta.`, m,rcanal);
   }
 
   try {
-    await m.react('🕓');
+    await m.react('');
 
     const apiUrl = `https://apis-starlights-team.koyeb.app/starlight/chatgpt?text=${encodeURIComponent(
       text
@@ -16,18 +16,16 @@ let handler = async (m, { conn, text }) => {
 
     if (!data || !data.result) {
       await m.react('❌');
-      return conn.reply(m.chat, `❀ No se pudo obtener una respuesta de la API.`, m);
+      return conn.reply(m.chat, `❀ No se pudo obtener una respuesta de la API.`, m,rcanal);
     }
 
-    await m.react('✅');
-    conn.reply(m.chat, `🤖 *Respuesta de la API*:\n\n${data.result}`, m);
+    conn.reply(m.chat, `🤖 ${data.result}`, m,rcanal);
   } catch (error) {
     console.error(error);
-    await m.react('⚠️');
     conn.reply(
       m.chat,
       `❀ Ocurrió un error al procesar tu solicitud. Por favor, inténtalo de nuevo más tarde.`,
-      m
+      m,rcanal
     );
   }
 };
