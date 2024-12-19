22e29d5ab1901d2c450bebf5595e2dde74e28c5a
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2024-12-19 02:31:37 -0500

    Delete plugins/obten-bot2.js

--- plugins/obten-bot2.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m, { conn }) => {
-    m.reply(m.chat, `${text}`, m, fake);
-};
-
-handler.command = /^(tenerbot2)$/i;
-handler.tags = ['serbot'];
-handler.help = ['tenerbot2 *<el bot para tu grupo>*'];
-
-export default handler;
